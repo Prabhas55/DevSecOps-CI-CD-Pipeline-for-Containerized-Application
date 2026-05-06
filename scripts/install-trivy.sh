d6303449b0830ef36ea01a4901b05bd38001b7e7
Prabhas Nalajala <prabhaschowdary14@gmail.com>
2026-05-06 18:05:25 +0530

    Update install-trivy.sh

--- scripts/install-trivy.sh
+++ scripts/install-trivy.sh
@@ -3,8 +3,6 @@
 # Trivy Installation Script
 # ============================================================
 
-set -e
-
 echo "==> Installing Trivy..."
 
 wget https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.tar.gz
@@ -12,8 +10,9 @@ tar zxvf trivy_0.18.3_Linux-64bit.tar.gz
 sudo mv trivy /usr/local/bin/
 
 # Add to PATH
+Vim .bashrc
 echo 'export PATH=$PATH:/usr/local/bin/' >> ~/.bashrc
-source ~/.bashrc
+source .bashrc
 
 echo ""
 trivy --version
